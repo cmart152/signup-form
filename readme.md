08062ebd49f333454879f0ee7596b0707bcd0ee0
Chester Martin <chester.martin89@gmail.com>
2022-11-16 14:34:38 +0000

    Completed signup form with working validations, hybrid of HTML and Javascript

---
+++ readme.md
@@ -0,0 +1,3 @@
+This signup form is part was created by myself to a specification provided by The Odin Project
+
+The aim is to showcase understanding of form layout, stlying and validation functions
---
+++ script.js
@@ -0,0 +1,34 @@
+document.querySelector("button").onclick = function(){
+  let password = document.querySelector('.password');
+  let cpassword = document.querySelector('.cpassword');
+  let incorrectPassword = document.getElementById('red-text')
+  const regEx = /(?=^.{7,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
+
+  if (incorrectPassword != null) {
+    password.classList.remove("invalid");
+    cpassword.classList.remove("invalid");
+    incorrectPassword.remove()
+  }
+
+  if (password.value != cpassword.value) {
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = "Passwords do not match";
+    newDiv.style.color = "red"
+    newDiv.classList.add("uber-small-font")
+    newDiv.setAttribute('id', 'red-text')
+    document.getElementById("confirm").appendChild(newDiv);
+    password.classList.add("invalid");
+    cpassword.classList.add("invalid");
+    return false
+  } else if (regEx.test(password.value)  == false) {
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = "Password does not match required pattern";
+    newDiv.style.color = "red"
+    newDiv.classList.add("uber-small-font")
+    newDiv.setAttribute('id', 'red-text')
+    document.getElementById("confirm").appendChild(newDiv);
+    password.classList.add("invalid");
+    cpassword.classList.add("invalid");
+    return false
+  }
+  }
